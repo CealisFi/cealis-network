ce4d7740173a15c1a2949bc73b9b589b868631a3
CealisFi <waqshata@gmail.com>
2026-07-08 08:34:13 +0700

    Create page.tsx

---
+++ dashboard/page.tsx
@@ -0,0 +1,20 @@
+import MarketCard from "@/components/MarketCard";
+import PortfolioCard from "@/components/PortfolioCard";
+
+export default function Dashboard() {
+
+  return (
+
+    <main>
+
+      <h1>Cealis Trading Dashboard</h1>
+
+      <MarketCard />
+
+      <PortfolioCard />
+
+    </main>
+
+  );
+
+}
